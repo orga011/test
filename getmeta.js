5c6f088e557038a80a306aa686b4c5fdc7a8b016
三上有紀 <orga011@gmail.com>
2018-01-09 12:49:10 +0900

    js修正

--- getmeta.js
+++ getmeta.js
@@ -1,16 +1,5 @@
 var url = location.href;
 var title = document.title;
 var description = document.getElementsByName ('description').item(0).content;
-var canonical_url = getCanonicalUrl();
-console.log(url + '☆' + title + '☆' + description + '☆' + canonical_url)
-function getCanonicalUrl() {
-    var links = document.getElementsByTagName("link");
-    for (var i = 0; i < links.length; i++) {
-        if (links[i].rel) {
-            if (links[i].rel.toLowerCase() == "canonical") {
-                    return links[i].href;
-            }
-        }
-    }
-    return null;
-}
+var kw = document.getElementsByName ('keywords').item(0).content;
+console.log(title + '\t' + description + '\t' + kw + '\t' + url);
