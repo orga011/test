16f26215c777dfae351db40b288e63018695c957
三上有紀 <orga011+git@gmail.com>
2018-10-25 22:09:12 +0900

    テストコミット

--- getmeta.js
+++ getmeta.js
@@ -3,3 +3,4 @@ var title = document.title;
 var description = document.getElementsByName ('description').item(0).content;
 var kw = document.getElementsByName ('keywords').item(0).content;
 console.log(title + '\t' + description + '\t' + kw + '\t' + url);
+// 
